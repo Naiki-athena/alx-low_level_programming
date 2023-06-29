bab7bc83d472474892d7e8b67a8de5cb18e995c1
Naiki-athena <patblessed24@gmail.com>
2023-06-29 15:23:26 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_taupper - changes all lowercase to uppercase
+ * @n: pointer
+ * Return: n
+ */
+char *string_taupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
